7deadce9cd28fa806e3c0b19ec7acb3dd561e23d
Maiko Morales <maiko.moralesr@gmail.com>
2020-07-02 22:50:34 -0400

    Added drawer navigator

--- App.tsx
+++ App.tsx
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
+import HomeScreen from './screens/HomeScreen';
+import CoursesScreen from './screens/CoursesScreen';
+import GalleryScreen from './screens/GalleryScreen';
+import AboutUsScreen from './screens/AboutUsScreen';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Inicio">
+        <Drawer.Screen name="Inicio" component={HomeScreen} />
+        <Drawer.Screen name="Cursos" component={CoursesScreen} />
+        <Drawer.Screen name="Galeria" component={GalleryScreen} />
+        <Drawer.Screen name="Quienes somos" component={AboutUsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
