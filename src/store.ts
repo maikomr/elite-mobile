76a6319493aefdda522a69cd35a98230d1b0a1a6
Maiko Morales <maiko.moralesr@gmail.com>
2020-07-24 23:55:28 -0400

    Added reducers to the root store and fixed filename admissionType -> admissionPeriod

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,20 @@
 import { combineReducers, createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import categories from "./state/categories/categories.reducer";
+
+import admissionPeriod from "./state/admissionPeriod/admissionPeriod.reducer";
+import admissionType from "./state/admissionType/admissionType.reducer";
+import career from "./state/career/career.reducer";
+import course from "./state/course/course.reducer";
+import faculty from "./state/faculty/faculty.reducer";
+import subject from "./state/subject/subject.reducer";
 
 const rootReducer = combineReducers({
-  categories,
+  admissionPeriod,
+  admissionType,
+  career,
+  course,
+  faculty,
+  subject,
 });
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
