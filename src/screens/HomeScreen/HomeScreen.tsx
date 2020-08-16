eec4995d60e4ac6df393079ca18ec8c1c99e668e
Maiko Morales <maiko.moralesr@gmail.com>
2020-08-16 10:09:54 -0400

    Added map

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Linking, StyleSheet, View, Dimensions } from 'react-native';
 import { Layout, Text, Button, Icon } from '@ui-kitten/components';
 import { DrawerScreenProps } from '@react-navigation/drawer';
+import MapView, { Marker } from 'react-native-maps';
+
 import ROUTES from '../../constants/routes';
+import { TouchableHighlight } from 'react-native-gesture-handler';
 
 const HomeScreen: React.FC<DrawerScreenProps<any>> = ({ navigation }) => (
   <Layout style={styles.container}>
@@ -21,16 +24,45 @@ const HomeScreen: React.FC<DrawerScreenProps<any>> = ({ navigation }) => (
       </Text>
     </View>
     <View style={styles.contactContainer}>
-      <View style={styles.contactItemContainer}>
-        <Icon name="phone-outline" style={styles.icon} fill='#8F9BB3' />
-        <Text>4483728</Text>
-      </View>
-      <View style={styles.contactItemContainer}>
-        <Icon name="smartphone-outline"  style={styles.icon} fill='#8F9BB3'/>
-        <Text>74837382</Text>
-      </View>
+      <TouchableHighlight
+        onPress={() => Linking.openURL(`tel:4483728`)}
+        underlayColor="#FAFBFC"
+      >
+        <View style={styles.contactItemContainer}>
+          <Icon name="phone-outline" style={styles.icon} fill="#8F9BB3" />
+          <Text>4483728</Text>
+        </View>
+      </TouchableHighlight>
+      <TouchableHighlight
+        onPress={() => Linking.openURL(`tel:74837382`)}
+        underlayColor="#FAFBFC"
+      >
+        <View style={styles.contactItemContainer}>
+          <Icon name="smartphone-outline" style={styles.icon} fill="#8F9BB3" />
+          <Text>74837382</Text>
+        </View>
+      </TouchableHighlight>
+    </View>
+    <View style={styles.mapContainer}>
+      <MapView
+        style={styles.mapStyle}
+        region={{
+          latitude: -17.392477,
+          longitude: -66.151015,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        }}
+        scrollEnabled={false}
+      >
+        <Marker
+          coordinate={{ latitude: -17.392477, longitude: -66.151015 }}
+          pinColor="red"
+          title="Instituto Elite"
+        />
+      </MapView>
     </View>
     <Button
+      style={styles.exploreBtn}
       onPress={() => navigation.navigate(ROUTES.COURSES.ROOT)}
       accessoryRight={(style) => (
         <Icon {...style} name="arrow-forward-outline" />
@@ -48,13 +80,13 @@ const styles = StyleSheet.create({
     padding: 20,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   titleContainer: {
     flexDirection: 'row',
   },
   subtitleContainer: {
-    padding: 25,
+    padding: 20,
   },
   subtitle: {
     textAlign: 'center',
@@ -67,18 +99,32 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center',
-    marginVertical: 20
+    marginVertical: 20,
   },
   contactItemContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   icon: {
     width: 32,
     height: 32,
-    marginRight: 10
-  }
+    marginRight: 10,
+  },
+  mapContainer: {
+    overflow: 'hidden',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#8F9BB3',
+    marginVertical: 20,
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width - 100,
+    height: Dimensions.get('window').width - 100,
+  },
+  exploreBtn: {
+    marginTop: 20,
+  },
 });
 
 export default HomeScreen;
