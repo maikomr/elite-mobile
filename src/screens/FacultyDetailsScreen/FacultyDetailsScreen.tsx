d1a21cf03a7755702f0a3eafa2348b773850f4da
Maiko Morales <maiko.moralesr@gmail.com>
2020-09-06 01:15:50 -0400

    Modified styles according to new design

--- src/screens/FacultyDetailsScreen/FacultyDetailsScreen.tsx
+++ src/screens/FacultyDetailsScreen/FacultyDetailsScreen.tsx
@@ -1,44 +1,54 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { StyleSheet, View } from 'react-native';
 import {
   Layout,
-  IndexPath,
+  // IndexPath,
   Text,
-  Select,
-  SelectItem,
+  // Select,
+  // SelectItem,
   Button,
   Icon,
 } from '@ui-kitten/components';
 import { StackScreenProps } from '@react-navigation/stack';
+import { Faculty } from '../../models/faculty';
 
 const FacultyDetailsScreen: React.FC<StackScreenProps<any>> = ({
   navigation,
   route,
 }) => {
-  const faculty = useMemo(() => route.params.faculty, [route.params.faculty]);
-  const [selectedIndex, setSelectedIndex] = useState<IndexPath | IndexPath[]>(
-    new IndexPath(0)
-  );
-  const enrollmentOptions = useMemo(
-    () =>
-      Object.keys(faculty.enrollmentOptions).sort((a, b) => a.localeCompare(b)),
-    [faculty.enrollmentOptions]
-  );
+  const faculty: Faculty = useMemo(() => route.params.faculty, [route.params.faculty]);
+  // const [selectedIndex, setSelectedIndex] = useState<IndexPath | IndexPath[]>(
+  //   new IndexPath(0)
+  // );
+  // const enrollmentOptions = useMemo(
+  //   () =>
+  //     Object.keys(faculty.enrollmentOptions).sort((a, b) => a.localeCompare(b)),
+  //   [faculty.enrollmentOptions]
+  // );
 
   useEffect(() => {
     navigation.setOptions({
       title: faculty.name,
+      headerStyle: styles.header,
     });
   }, [faculty.name]);
 
-  const value = enrollmentOptions[(selectedIndex as IndexPath).row];
-  const getEnrollmentOption = (key: string) =>
-    `${key} - ${faculty.enrollmentOptions[key]} Bs.`;
+  // const value = enrollmentOptions[(selectedIndex as IndexPath).row];
+  // const getEnrollmentOption = (key: string) =>
+  //   `${key} - ${faculty.enrollmentOptions[key]} Bs.`;
 
   return (
     <Layout style={styles.container}>
+      <Button
+        onPress={() => {}}
+        accessoryRight={(style) => (
+          <Icon {...style} name="checkmark-circle-outline" />
+        )}
+      >
+        Inscribirse
+      </Button>
       <Text style={styles.subtitle} category="h6">
-        Modalidad de Admisión
+        {`Modalidad${faculty.admissionTypes.length > 1 ? 'es' : ''} de Admisión`}
       </Text>
       <View>
         {faculty.admissionTypes.map((admissionPeriod: string) => (
@@ -48,7 +58,7 @@ const FacultyDetailsScreen: React.FC<StackScreenProps<any>> = ({
         ))}
       </View>
       <Text style={styles.subtitle} category="h6">
-        Fecha de Admisión
+        {`Fecha${faculty.admissionPeriods.length > 1 ? 's' : ''} de Admisión`}
       </Text>
       <View>
         {faculty.admissionPeriods.map((admissionPeriod: string) => (
@@ -61,7 +71,7 @@ const FacultyDetailsScreen: React.FC<StackScreenProps<any>> = ({
         Descripción
       </Text>
       <Text>{faculty.description}</Text>
-      <Text style={styles.subtitle} category="h6">
+      {/* <Text style={styles.subtitle} category="h6">
         Duración
       </Text>
       <Select
@@ -73,18 +83,36 @@ const FacultyDetailsScreen: React.FC<StackScreenProps<any>> = ({
         {enrollmentOptions.map((key) => (
           <SelectItem key={key} title={getEnrollmentOption(key)} />
         ))}
-      </Select>
+      </Select> */}
       <Button
         onPress={() => {}}
-        accessoryRight={(style) => <Icon {...style} name="checkmark-circle-outline" />}
+        appearance="outline"
+        style={styles.secondaryButton}
+        accessoryRight={(style) => (
+          <Icon {...style} name="arrow-ios-forward-outline" />
+        )}
       >
-        INSCRIBIRSE
+        Ver carreras
+      </Button>
+      <Button
+        onPress={() => {}}
+        appearance="outline"
+        style={styles.secondaryButton}
+        accessoryRight={(style) => (
+          <Icon {...style} name="arrow-ios-forward-outline" />
+        )}
+      >
+        Ver materias
       </Button>
     </Layout>
   );
 };
 
 const styles = StyleSheet.create({
+  header: {
+    elevation: 0,
+    shadowOpacity: 0,
+  },
   container: {
     flex: 1,
     padding: 20,
@@ -94,10 +122,14 @@ const styles = StyleSheet.create({
     marginTop: 20,
     marginBottom: 5,
   },
-  selectDuration: {
-    marginTop: 5,
-    marginVertical: 40,
-  },
+  // selectDuration: {
+  //   marginTop: 5,
+  //   marginVertical: 40,
+  // },
+  secondaryButton: {
+    marginTop: 30,
+    justifyContent: 'space-between'
+  }
 });
 
 export default FacultyDetailsScreen;
